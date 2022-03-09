9ef69df5933fa77534672291be32083947b513d7
danielaMar13 <101075799+danielaMar13@users.noreply.github.com>
2022-03-08 19:02:57 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def getTasaConversion(tipo, tipoCambio):
     tasaSol = {"Peso": 5.71, "Dolar": 0.27, "Yen": 30.94, "Euro": 0.25, "Won": 329.94, "Bolivar": 1.16, "Quetzal": 2.07, "Yuan": 1.69, "Rupia": 20.58}
     tasaBolivar = {"Peso": 4.94, "Dolar": 0.23, "Yen": 26.76, "Euro": 0.21, "Won": 285.27, "Sol": 0.86, "Quetzal":1.78, "Yuan": 1.46, "Rupia": 17.79}
     tasaQuetzal =  {"Peso": 2.78, "Dolar": 0.13, "Yen": 15.05, "Euro": 0.12, "Won": 160.39, "Sol": 93.80, "Bolivar": 0.49, "Yuan": 0.82, "Rupia": 10.00}
-    tasaYuan = {"Peso": 3.38, "Dolar": 0.16, "Yen": 18.31, "Euro": 0.15, "Won":0.00074, "Sol": 0.0030, "Bolivar": 0.0035, "Quetzal": 0.0062, "Rupia": 0.062}
+    tasaYuan = {"Peso": 3.38, "Dolar": 0.16, "Yen": 18.31, "Euro": 0.15, "Won":0.00074, "Sol": 0.0030, "Bolivar": 0.0035, "Quetzal": 1.22, "Rupia": 0.062}
     tasaRupia = {"Peso": 0.28, "Dolar": 0.013, "Yen": 1.50, "Euro": 0.012, "Won": 16.03, "Sol": 0.049, "Bolivar": 0.056, "Quetzal": 0.10, "Yuan": 0.082}
 
     tasa = 0.0
